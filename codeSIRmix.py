8f69bcd580224e4922361d3f68a6d6f35ddcf0e5
Joelle Murray <jmurray@linfield.edu>
2020-06-18 16:15:55 -0700

    updated q range and axis titles

--- codeSIRmix.py
+++ codeSIRmix.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Tue Feb 18 14:48:58 2020
+Created on Tue May 5 14:48:58 2020
 
 @author: jmurray
 """
@@ -13,6 +13,10 @@ Created on Tue Feb 18 14:48:58 2020
 # https://www.davidketcheson.info/2020/03/19/SIR_Estimating_parameters.html
 # https://www.davidketcheson.info/2020/03/19/SIR_predictions.html
 # basic assumption beta = 0.25 and gamma = 0.06
+# added in 2 different susceptible populations: S1 and S2
+# S1 uses physical distancing to change the beta to q*beta where q = 0 for 
+# full PD (total isolation) and q=1 for no PD
+# while S2 never uses PD
 
 # importing packages
 import numpy as np
@@ -26,7 +30,6 @@ import math
 dt = 0.01
 # average number of people that come within infection range of an infected individual
 # per day
-# ASSUMES NO SOCIAL DISTANCING OR MITIGATION
 beta = 0.25
 # remove probability per day = 1/(recovery time)
 # approximately 16 days (varies from 14 days to a month)
@@ -42,11 +45,11 @@ pr = 0.0
 pi = 0.01
 # initial percent susceptible
 ps = 1-pr-pi
-# percent isolators (social distance)
-psd = 0.5
+# percent isolators (physical distance = PD)
+ppd = 0.5
 # plotting
 fig = plt.figure()
-for i in range(11):
+for i in range(0,11):
 # social distance factor (0 = total isolation, 1 = no isolation)
     q = 0.1*(i-1)
     
@@ -60,11 +63,12 @@ for i in range(11):
 # initial populations
  
     S[0] = ps*N
-    S1[0]= psd*S[0]
+    S1[0]= ppd*S[0]
     S2[0]= S[0]-S1[0]
     I[0] = pi*N
+
     
-    for j in range(niter-1):
+    for j in range(niter-1):        
         dS1dt=-q*beta/N*S1[j]*I[j]
         dS2dt=-beta/N*S2[j]*I[j]
         dIdt=-dS1dt-dS2dt-gamma*I[j]
@@ -83,13 +87,12 @@ for i in range(11):
 #plt.plot(t, R, 'b', label = 'recovered')
 #plt.plot(t, R+S+I, 'y', label = 'total')
 #    plt.gca().legend(('susceptible','infected','recovered','total'))
-    plt.title(r'SIR mix ode model with $\beta$  = 0.25, $\gamma$ = 0.05')
+    plt.title(r'codesirmix model with N = 1E6, $\beta$  = 0.25, $\gamma$ = 0.05')
     plt.xlabel('days elapsed since 1 percent of the population became infected')
-    plt.ylabel('population')
+    plt.ylabel('total infected population')
 
-#    plt.annotate('hello')
-#    plt.annotate('q = 1.0', ha = 'center', va = 'bottom', xytext = 
-#                 (10, 3.5),xy = (0.69, 0),arrowprops = {'facecolor' : 'black'})
+
+
 plt.annotate('q = 0.0', ha = 'center', va = 'bottom', xytext = (1.3E2, 1.1E5), 
              xy = (60, 0.8E5),arrowprops = {'facecolor' : 'black'})
 plt.annotate('q = 1.0', ha = 'center', va = 'bottom', xytext = (1.2E2, 4.0E5), 
